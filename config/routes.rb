3af227fa5ff6cf0932565c5cd1dc6dbfa36371f1
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-27 14:18:20 +0100

    Add resource to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root : 'new#index'
+  resources :users
 end
