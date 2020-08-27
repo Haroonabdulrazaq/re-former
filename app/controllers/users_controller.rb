a4da80fb3f480c6f2a4aae7292891f8caa5306f1
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-27 16:12:16 +0100

    Add content to create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(username: params[:username], email: params[:email],password: params[:password])
+
+        if @user.save
+           redirect to new_user_path
+        else
+           render :new
+        end
     end
 end
